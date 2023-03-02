8e506d5fff2d91bbebe42f425ff148ec9414236d
jewelghosh6 <jewelghosh611@gmail.com>
2023-03-02 17:55:31 +0530

    First commit

---
+++ src/test/java/com/jewel/resumeportal/ResumePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewel.resumeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
